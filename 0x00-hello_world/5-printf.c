02ece1a01f477cbc29172307c04bb40e493d4421
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-08 12:58:13 +0000

    print a string followd by newline using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - print string followed by a newline using printf
+ *Return: return 0 if function runs succesfully
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
+}
