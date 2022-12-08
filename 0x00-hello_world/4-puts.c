169d435ccaf7a7d65c36dfeff0b015b07149fbf3
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-08 12:04:08 +0000

    printing with the puts method

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
+/*display a string to the terminal followed by a new line*/
+
 #include <stdio.h>
 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
- return(0);
+return(0);
 }
 
