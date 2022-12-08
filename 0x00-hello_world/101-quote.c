aba1c2747c5c3b435ee21f67efbd0d1c1693fd5c
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-08 13:55:37 +0000

    print a string to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-
-int main(void){
-fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+/**
+ *main - a function that's print a string to standard error
+ *Return: 1 means there was an error
+ */
+int main(void)
+{
+fprintf(stderr,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
