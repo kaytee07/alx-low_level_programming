6efba54078fe3a2fe35956851b7eb7feec6498f0
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-09 12:41:20 +0000

    print combinations of single numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%i: is positive\n", n);
+printf("%i is positive\n", n);
 else if (n == 0)
-printf("%i: is zero\n", n);
+printf("%i is zero\n", n);
 else
-printf("%d: is negative\n", n);
+printf("%d is negative\n", n);
 
 return (0);
 }
