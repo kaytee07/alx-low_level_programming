5f478cfa767beaffed5c70082b52caac135603d7
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-09 13:20:17 +0000

    3 combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - print the combinations of single digit numbers
+ *
+ *Return: 0 if function runs successfully
+ */
+
+int main(void)
+{
+int comb;
+int nxtnum;
+for (comb = 0 ; comb < 10 ; comb++)
+{
+if(comb == 9)
+{
+break;
+}
+for (nxtnum = comb+1 ; nxtnum < 10 ; nxtnum++)
+{
+putchar(comb + '0');
+putchar(nxtnum + '0');
+if(comb < 8)
+{
+putchar(',');
+putchar(' ');  
+}
+}
+}
+putchar('\n');
+return (0);
+}
