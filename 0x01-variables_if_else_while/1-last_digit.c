3a6cb6ea7dbf93b8399e91b3c0b6ee35927ebefb
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-09 08:25:04 +0000

    check the range the last number of a random number falls within

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ *main - print and determine range of last number
+ *
+ *Return: 0 if function run sucessfully
+ */
+
+int main(void)
+{
+srand(time(0));
+int n = rand() - RAND_MAX / 2;
+if (n % 10 > 5)
+printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
+else if (n % 10 == 0)
+printf("Last digit of %i is %i and is 0\n", n, n % 10);
+else
+printf("Last digit of %i is %i and is less than 6\n", n, n % 10);
+return (0);
+}
