71a788772c1de7cbf6368a7bdf560e0ced215a26
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-09 12:43:47 +0000

    corrections

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,8 +11,9 @@
 
 int main(void)
 {
+int n;
 srand(time(0));
-int n = rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 if (n % 10 > 5)
 printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 else if (n % 10 == 0)
