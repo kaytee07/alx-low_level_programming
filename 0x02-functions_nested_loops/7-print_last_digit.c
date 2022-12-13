5d1862801c04132fdb6990c6b10909e0c947579f
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-13 17:27:40 +0000

    return the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <limits.h>
 /**
 * print_last_digit - print the last digit of a number
 * @a: first parameter
@@ -13,7 +13,13 @@ int print_last_digit(int a)
 if (a < 0)
 a = -a;
 
-_putchar((a % 10) + '0');  
+_putchar((a % 10) + '0');
 return (a % 10);
+}
+
+int main()
+{
+print_last_digit(INT_);
+return 0;
 }
 
