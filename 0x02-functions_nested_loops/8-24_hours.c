43143230edf033c179d292e93cb38e7d959a5b57
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-13 16:22:16 +0000

    print 9 times table

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+
+
+void jack_bauer(void)
+{
+time_t seconds;
+struct tm *timestruct;
+seconds = time(NULL);
+printf("%d:%d", timeStruct->tm_hour, timeStruct->tm_min);
+}
