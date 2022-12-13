1a5b7751b073147a86ee4621da2c2575e2569f19
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-13 13:21:41 +0000

    print argument passed to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_to_98(int n)
+{
+while (n < 99)
+{
+_putchar(n);
+if (n < 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+n++
+}
+_putchar('\n');
+}
+
+
