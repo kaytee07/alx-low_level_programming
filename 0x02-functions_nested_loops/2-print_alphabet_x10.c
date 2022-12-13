5892f1b2e8d277a3d95b7a9e53e5e54273842263
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-13 12:45:47 +0000

    removed main.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,12 @@ void print_alphabet_x10(void)
 {
 int i;
 int j;
-for(j = 0 ; j <= 10 ; j++)
+for (j = 0 ; j < 10 ; j++)
 {
 for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
-if (j < 10)
 _putchar('\n');
 }
 }
