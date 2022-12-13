c55e6ec261f667b1fda56764c634ee7898d17cf7
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-13 14:46:21 +0000

    print the 9 times table starting with 0

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -9,7 +9,7 @@
  *Return: a + b
  */
 
-int add (int a, int b)
+int add(int a, int b)
 {
-return a + b ;
+return (a + b);
 }
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+
+void times_table(void)
+{
+int i;
+int j;
+int q;
+q= 0;
+
+for(i = 0 ; i <= 9 ; i++)
+{
+q=0;
+printf("%d,  ", q);
+for(j = 0 ; j < 9 ; j++)
+{
+q = q + i;
+printf("%d", q);
+
+if (j < 8)
+if (q <= 9)
+printf(",  ");
+else
+printf(", ");
+
+}
+_putchar('\n');
+}
+}
+
+
