e9daeee0c55fa1c99def22c154aabf3ab33beca3
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-13 13:57:58 +0000

    print argument to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,32 @@
 void print_to_98(int n)
 {
 int i;
+if (n > 98){
+for (i = 98 ; i >= 98 ; i--)
+{
+print("%d", i);  
+if (i <= 97)
+printf(", ");
+ 
+}
+}
+else if(n < 98)
+{
+
 for (i = n ; i < 99 ; i++)
 {
+  
 printf("%d", i);
 if (i <= 97)
 printf(", ");
+ 
 }
+ 
+}
+else
+printf("%d", n)
+  
+   
 _putchar('\n');
 }
 
