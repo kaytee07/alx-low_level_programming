d084141a8af8e1d4f6c7a6eadceb52f6066a9e70
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-14 08:11:53 +0000

    debugging code to print out day and year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -34,10 +34,6 @@ printf("Remaining days: %d\n", 366 - day);
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
-else
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
 }
 }
 
