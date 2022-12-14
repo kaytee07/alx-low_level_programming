c3896c528460043508a46cb03ef1c1d5f27ba46c
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-14 06:28:04 +0000

    dfixed the comparison to find the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,20 +12,19 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 printf("%d, %d, %d\n", a, b, c);
-if (a > b && a > c)
+if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b > a && b > c)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
-else if (c > a && c > b)
+else if (c >= a && c >= b)
 {
 largest = c;
 }
 
 return (largest);
 }
-
 
