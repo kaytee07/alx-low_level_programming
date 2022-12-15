3ccd210228917abc8080631da6d0d9be2020da45
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-15 16:51:14 +0000

    remove last space fizz buzz function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,24 +16,28 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
+if (i != 100)
 putchar(' ');
 continue;
 }
 if (i % 3 == 0)
 {
 printf("Fizz");
+if (i != 100)
 putchar(' ');
 continue;
 }
 if (i % 5 == 0)
 {
 printf("Buzz");
+if (i != 100)
 putchar(' ');
 continue;
 }
 if (i % 3 != 0 && i % 5 != 5)
 {
 printf("%d", i);
+if (i != 100)
 putchar(' ');
 }
 }
