ca0de2a4e5093c4b709d69880a49e970baff4530
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-15 09:34:09 +0000

    print n number of underscores where n is the parameter passed

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+/**
+ * print_line - print the nth number of line
+ * @n: first argument
+ *
+ */
+
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i <= n; i++)
+{
+putchar('_');
+}
+putchar('\n');
+}
+else
+putchar('\n');
+}
