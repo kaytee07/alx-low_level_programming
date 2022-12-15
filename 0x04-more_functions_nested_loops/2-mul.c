184ac48703d46a5ccc09a5cbca8e0c6020b0d34c
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-15 07:43:58 +0000

    function that multiplies and return the two parameters passed to iy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+/**
+ * mul - function multiplies two integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: 0 if function execute succesfully
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
