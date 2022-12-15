4e7a34f14eb88675885013a0d7cf81e775feb045
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-15 17:52:38 +0000

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,36 +12,26 @@
 
 void print_triangle(int size)
 {
-  int j;
-  int i;
-  int k;
+int j;
+int i;
+int k;
 for (i = 1; i <= size; i++)
 {
-  k = i;
+k = i;
 for (j = 1; j <= size; j++)
-  {
-    if(k && j > size - k)
-      {
-    putchar('#');
-    k--;
-      }
-    else
-      {
-	putchar(' ');
-      }
-    
-  }
- putchar('\n');
-}
-}
-
-
-int main()
 {
-  print_triangle(2);
-  print_triangle(10);
-  print_triangle(1);
-  print_triangle(0);
-  return (0);
+if (k && j > size - k)
+{
+putchar('#');
+k--;
 }
+else
+{
+putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+
 
