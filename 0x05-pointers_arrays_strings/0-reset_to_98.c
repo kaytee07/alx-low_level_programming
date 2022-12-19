1ebba79e558a672bbe375d590eade6c7d9ddc04c
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-19 07:42:46 +0000

    resetting n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates the value to 98
+ * @n: pointer variable argument
+ */
+
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
