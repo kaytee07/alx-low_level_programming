1d76a50a4d3ea5fcaad8b4670e4d66c0717c1813
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-19 19:04:13 +0000

    convert string to number

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,19 +13,12 @@ int sign = 1;
 
 do {
 if (*s == '-')
-{
 sign *= -1;
-}
 else if (*s >= '0' && *s <= '9')
-{
 num = (num * 10) + (*s - '0');
-}
 else if (num > 0)
-{
 break;
-}
-}
-while (*s++);
+} while (*s++);
 
-return (num * sign);
+return (num *sign);
 }
