4e0c59e07fc767e9de6412d5f0d5fcd2767cb840
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-19 14:28:13 +0000

    copy string from source to destination

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,18 +13,29 @@
 void print_array(int *a, int n)
 {
 int i;
-i = 0;
 for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
-if (i < n - 1)
+if (i != n - 1)
 {
-_putchar(',');
-_putchar(' ');
+putchar(',');
+putchar(' ');
 }
 
 }
-_putchar('\n');
+putchar('\n');
 }
 
 
+int main(void)
+{
+    int array[5];
+
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    print_array(array, 5);
+    return (0);
+}
