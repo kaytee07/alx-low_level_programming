210f83e4542c531701bf53c90791839ba052133a
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-19 08:15:50 +0000

    print a string to put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _puts - print a string to stdout
+ * @str: first argumanent which is a pointer to a string
+ *
+ *
+ */
+
+void _puts(char *str)
+{
+puts(str);
+}
