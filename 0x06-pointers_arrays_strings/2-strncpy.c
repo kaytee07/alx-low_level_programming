bfba72be748d565411b8de48feb4cf6b7aa08d6f
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-22 12:27:19 +0000

    copy n number of strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _strncat - copy string to dest
+ * _strncpy - copy string to dest
  * @dest: first parameter
  * @src: second parameter
  * @n: third parameter
@@ -10,17 +10,16 @@
  */
 
 
+
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
-while(i < n && src[i] != '\0')
-{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
+while (i < n)
+{
+dest[i] = '\0';
 i++;
 }
-for ( ; i < n; i++)
-dest[i] = '\0';
 return (dest);
 }
-
-    
