9e373ce1205e6eedfe992beb330fa42412a97074
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-22 08:44:32 +0000

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,18 +4,22 @@
  * print_number - Prints an integer.
  * @n: The integer to be printed.
  */
+
 void print_number(int n)
 {
-unsigned int num = n;
+unsigned int n1;
+
+n1 = n;
 
 if (n < 0)
 {
 _putchar('-');
-num = -num;
+n1 = -n;
 }
 
-if ((num / 10) > 0)
-print_number(num / 10);
-
-_putchar((num % 10) + '0');
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
 }
