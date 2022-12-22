e2a920459849c471aea8a7c9aa6c1612fde0dbdc
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-22 08:39:15 +0000

    print number passed as parameter

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
-  * print_number - print numbers
-  * @n: number to be printed
-  * Return: 0
- **/
-
-
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
 void print_number(int n)
 {
 unsigned int num = n;
+
 if (n < 0)
 {
 _putchar('-');
 num = -num;
 }
+
 if ((num / 10) > 0)
-{
 print_number(num / 10);
-}
+
 _putchar((num % 10) + '0');
 }
