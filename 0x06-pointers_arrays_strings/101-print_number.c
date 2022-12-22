3b658bcebbdcc181f1eafc811de6c8c52fba1295
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-22 15:22:34 +0000

    prit numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,14 +1,13 @@
+#include <stdio.h>
 #include "main.h"
-#include <stdio.h> 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - prints integer
+ * @n: input integer
+ * Return: no return
  */
-
 void print_number(int n)
 {
 unsigned int m, d, count;
-
 if (n < 0)
 {
 _putchar(45);
@@ -27,8 +26,11 @@ while (d > 9)
 d /= 10;
 count *= 10;
 }
+
 for (; count >= 1; count /= 10)
 {
 _putchar(((m / count) % 10) + 48);
 }
 }
+
+
