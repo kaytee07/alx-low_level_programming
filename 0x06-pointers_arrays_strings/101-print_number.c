50fa323517c25cd0183244dfcb5681d0402d8fea
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2022-12-22 15:51:11 +0000

    function adds two numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,30 +7,30 @@
  */
 void print_number(int n)
 {
-unsigned int m, d, count;
-if (n < 0)
-{
-_putchar(45);
-m = n * -1;
-}
-else
-{
-m = n;
-}
+	unsigned int m, d, count;
 
-d = m;
-count = 1;
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
 
-while (d > 9)
-{
-d /= 10;
-count *= 10;
-}
+	d = m;
+	count = 1;
 
-for (; count >= 1; count /= 10)
-{
-_putchar(((m / count) % 10) + 48);
-}
-}
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
 
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+}
 
