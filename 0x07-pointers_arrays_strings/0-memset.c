4ad5ba340769f6bd42bb990cf7b649e1dcec9a2c
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-03 16:06:49 +0000

    corrections

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _memset - fills the first n byte of memory area pointed to by s with b
  * @s: memory area to be filled
  * @b: value to fill s with
  * @n: the amount of memory in byte to fill
- * Return: s
+ * Return: pointer to the filled memory
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -18,6 +17,3 @@ for (i = 0; i < n; i++)
 }
 return (s);
 }
-
-
-
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -21,6 +21,20 @@ break;
 i++;
 }
 
-return (s[i] == '\0' ? NULL : &s[i]);
+ return (s[i] == c ? &s[i] : NULL);
+}
+
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
+    return (0);
 }
 
