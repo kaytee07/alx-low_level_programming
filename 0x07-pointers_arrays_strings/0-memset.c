e9a04f7df80a85d039bfe5ba594f46afdf20c9a7
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-03 18:28:48 +0000

    copy n byte from source to destination

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,20 @@
  * Return: pointer to the filled memory
  */
 
-char *_memset(char *s, char b, unsigned int n)
+ar *_memset(char *s, char b, unsigned int n)
 {
+	/**
+	 * declare an unsigned int
+	 * because we are storing a
+	 * value that will always be
+	 * non-negative (zero or positive)
+	*/
 unsigned int i;
+
 for (i = 0; i < n; i++)
 {
 s[i] = b;
 }
+
 return (s);
 }
