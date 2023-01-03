cd5f9674926c80d20782e57e28007b57cc4ad1d8
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-03 16:07:38 +0000

    corrections

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -21,20 +21,8 @@ break;
 i++;
 }
 
- return (s[i] == c ? &s[i] : NULL);
+return (s[i] == c ? &s[i] : NULL);
 }
 
-int main(void)
-{
-    char *s = "hello";
-    char *f;
 
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
-}
 
