d3595670913f68d9de5650d4b5201890993bb8b0
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-03 18:46:33 +0000

    add readme file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,13 +9,13 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int index;
-unsigned char *memory = s, value = c;
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
 
-for (index = 0; index < n; index++)
-memory[index] = value;
-
-return (memory);
+return (s);
 }
