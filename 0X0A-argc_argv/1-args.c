60274163b25429e8324765fb616c51d7e26b8459
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-09 10:42:37 +0000

    print number of command line args passed

---
+++ 0X0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print number of argument passed to main
+ * @argc: number of argument passed to main;
+ * @argv: array of argument passed to main
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
