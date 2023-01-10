48ee6e4bae74440d8a89af5764c8de02a4e964b4
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-10 11:45:01 +0000

    create an array and initialize it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array - create and array and initialize it to a value
+ * @size: this is the number * data type of memory to allocate
+ * @c: this is the value to initialize the array with
+ * Return: a pointer to the first value
+ */
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int i = 0;
+char *ptr = (char *) malloc(size * sizeof(char));
+for (; i < size; i++)
+{
+*(ptr + i) = c;
+}
+return (ptr);
+}
+
