b0e01e481efd15c39762178be21a4208df29b5d2
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-10 13:00:33 +0000

    copies string to a newly alocated space

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - copies string from parameter to a new pointer
+ * @str: pointer to string to be copied
+ * Return: pointer to copied string
+ *
+ *
+ */
+
+
+char *_strdup(char *str)
+{
+int i = 0;
+int j = 0;
+char *ptr;
+if (str == NULL)
+{
+return (NULL);
+}
+while (*(str + i) != '\0')
+{
+i++;
+}
+
+ptr = (char *) malloc(sizeof(char) * (i - 1));
+for (; j < i; j++)
+{
+*(ptr + j) = str[j];
+}
+return (ptr);
+}
+
