5fad3acd2ac65ae0735542bc35e05a46c9d974a7
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-12 10:27:48 +0000

    allocate memory of a specified size

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - function that allocate memory of specified size
+ * @b: specifies the number of byte of memory to allocate
+ * Return: pointer to reserved memory
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
+}
