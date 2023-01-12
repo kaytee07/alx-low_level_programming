9032a32cafdf060ff87b664fe0bfa8eaed5859bb
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-12 23:21:42 +0000

    multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
-
 int check_valid_input(char *num1, char *num2);
-long int multiply(char *num1, char *num2);
-size_t _strlen(const char *s);
+char *multiply(char *num1, char *num2);
 
 /**
  * main - multiplies two positive numbers
@@ -16,7 +15,7 @@ size_t _strlen(const char *s);
 
 int main(int argc, char *argv[])
 {
-long int result;
+char *result;
 if (argc != 3)
 {
 printf("Error\n");
@@ -28,26 +27,27 @@ printf("Error\n");
 exit(98);
 }
 result = multiply(argv[1], argv[2]);
-printf("%ld\n", result);
+printf("%s\n", result);
+free(result);
 return (0);
 }
 
 /**
- * check_valid_input - check if parameters are positie integers
- * @num1: first value to be multiplied
- * @num2: second value to be multiplied
- * Return: 0 if sucessfull and 1 if otherwise
+ * check_valid_input - check if parameters is a digit
+ * @num1: first parameter
+ * @num2: second parameter
+ * Return: 0 is successful
  */
 
 int check_valid_input(char *num1, char *num2)
 {
-size_t i;
-for (i = 0; i < _strlen(num1); i++)
+int i;
+for (i = 0; i < (int) strlen(num1); i++)
 {
 if (!isdigit(num1[i]))
 return (1);
 }
-for (i = 0; i < _strlen(num2); i++)
+for (i = 0; i < (int) strlen(num2); i++)
 {
 if (!isdigit(num2[i]))
 return (1);
@@ -55,31 +55,49 @@ return (1);
 return (0);
 }
 
-
 /**
  * multiply - multiplies parameters passed
  * @num1: first parameter
  * @num2: second parameter
  * Return: return multiplication of both numbers
  */
-long int multiply(char *num1, char *num2)
+
+char *multiply(char *num1, char *num2)
 {
-long int a = atol(num1);
-long int b = atol(num2);
-return (a * b);
+int i, j, k, carry = 0, num1Len, num2Len, productLen;
+int *product;
+char *result;
+num1Len = strlen(num1);
+num2Len = strlen(num2);
+productLen = num1Len + num2Len;
+product = (int *)malloc(productLen *sizeof(int));
+for (i = 0; i < productLen; i++)
+product[i] = 0;
+for (i = num1Len - 1; i >= 0; i--)
+{
+carry = 0;
+k = productLen - (num1Len - i);
+for (j = num2Len - 1; j >= 0; j--)
+{
+product[k] += (num1[i] - '0') * (num2[j] - '0') + carry;
+carry = product[k] / 10;
+product[k] %= 10;
+k--;
+}
+product[k] += carry;
+}
+i = 0;
+while (i < productLen && product[i] == 0)
+i++;
+result = (char *)malloc((productLen - i + 1) * sizeof(char));
+j = 0;
+while (i < productLen)
+{
+result[j++] = product[i++] + '0';
+}
+result[j] = '\0';
+free(product);
+return (result);
 }
 
-/**
- * _strlen - return the space occupied by the string
- * @s: string to find the length for
- * Return: size in byte
- */
 
-
-size_t _strlen(const char *s)
-{
-size_t len = 0;
-while (*s++)
-len++;
-return (len);
-}
