086ea224a12afcb269bf32108f585f054e548d95
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-12 14:55:44 +0000

    calloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,37 +13,40 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-size_t len1 = strlen(s1);
-size_t len2 = strlen(s2);
-char *result;
+unsigned int l1, l2, i, j;
+char *str;
+
+
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-if (n >= len2)
-{
-result = (char *) malloc(len1 + len2 + 1);
-}
-else
-{
-result = (char *) malloc(len1 + n + 1);
-}
-if (result == NULL)
-{
+
+l1 = l2 = 0;
+
+while (s1[l1] != '\0')
+l1++;
+while (s2[l2] != '\0')
+l2++;
+
+if (n >= l2)
+n = l2;
+
+str = (char *) malloc((l1 + n + 1) * sizeof(char));
+if (str == NULL)
 return (NULL);
-}
 
-strcpy(result, s1);
-if (n >= len2)
-{
-strcat(result, s2);
-}
-else
-{
-strncat(result, s2, n);
-}
 
-return (result);
+for (i = 0; s1[i] != '\0'; i++)
+str[i] = s1[i];
+
+for (j = 0; j < n && s2[j] != '\0'; j++)
+{
+str[i] = s2[j];
+i++;
+}
+str[i] = '\0';
+return (str);
 }
 
 
