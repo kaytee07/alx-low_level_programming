3d11b2afce0edfa4e64bd5e6ef024113220b62bc
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-18 17:53:35 +0000

    simple calculator funtions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,25 +8,38 @@
  * @argv: array of arguments
  * Return: result of the operation
  */
+
+
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
-int (*operation)(int, int);
+int num1, num2;
+char *operator;
 
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
+
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
-operation = get_op_func(argv[2]);
-if (operation == NULL)
+operator = argv[2];
+
+if (get_op_func(operator) == NULL || operator[1] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
-result = operation(num1, num2);
-printf("%d\n", result);
+
+if ((*operator == 47 || *operator == 37) && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+printf("%d\n", get_op_func(operator)(num1, num2));
+
 return (0);
 }
+
+
