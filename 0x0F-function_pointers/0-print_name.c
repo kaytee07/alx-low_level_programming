ea9dc46cd0f007954d811f701031075fab406441
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-18 13:16:33 +0000

    print out name either as it is or in caps

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print name either as it is or in caps
+ * @name: name to be printed out
+ * @f: callback function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
+
