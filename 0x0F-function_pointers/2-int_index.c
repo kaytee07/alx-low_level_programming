63d3cb7b806a516a438cb6506c11bf3ca0b58200
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-18 14:30:51 +0000

    check for the presence of all params in task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 int i;
 if (size < 0)
 return (-1);
-
+if (array != NULL && size > 0 && cmp != NULL)
+{
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]))
 return (i);
+}
 }
 
 return (-1);
