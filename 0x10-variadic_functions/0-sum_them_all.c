bef70e11a8d5153f221e9268ec1169cd67a1f4f1
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-19 11:42:25 +0000

    all variadic functions assignement

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all the parameters
+ * @n: this is the number of parameters passed
+ * @...: rest of the variables
+ * Return: total of addition
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i, sum = 0;
+va_list list;
+if (n == 0)
+{
+return (0);
+}
+va_start(list, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(list, int);
+}
+va_end(list);
+return (sum);
+}
