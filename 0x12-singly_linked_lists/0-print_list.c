061183301b23a28af3703bbfe59fb4c437a2f79c
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-26 15:00:30 +0000

    add new node to beginning of list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "list.h"
+#include "lists.h"
 
 /**
  * print_list - print elements in a linked list
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - add node to the beginning of the list
+ * @head: address to the beginning of the list
+ * @str: string to add to new node string
+ * Return: addrss of the new head node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+if (head == NULL || str == NULL)
+return (NULL);
+list_t *new;
+new = malloc(sizeof(list_t));
+new->str = strdup(str);
+new->len = strlen(str);
+new->next = *head;
+*head = new;
+return (new);
+}
