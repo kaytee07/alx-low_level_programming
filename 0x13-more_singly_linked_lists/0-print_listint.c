d383e5b42509b933e9808afa2ccdb31f9ca76149
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-30 23:57:53 +0000

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,6 +3,26 @@
 #include "lists.h"
 
 /**
+ * printnum - print digit with putchar
+ * @n: digit to print with putchar
+ *
+ */
+void printnum(int n)
+{
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
+
+if (n / 10)
+printnum(n / 10);
+
+putchar(n % 10 + '0');
+putchar('\n');
+}
+
+/**
  * print_listint - print the elements of all nodes
  * @h: address to head of linked list
  * Return: number of nodes
@@ -10,14 +30,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-int count = 0;
+int i = 0;
 
 while (h != NULL)
 {
-printf("%d\n", h->n);
-count++;
+printnum(h->n);
 h = h->next;
+i++;
 }
 
-return (count);
+return (i);
 }
