9ae0834820629611aa81d108b0d8ec8ab23f3252
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-31 00:36:03 +0000

    add

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,14 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 
 typedef struct listint_s
@@ -16,6 +15,19 @@ typedef struct listint_s
 int n;
 struct listint_s *next;
 } listint_t;
+
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+void *p;
+struct listp_s *next;
+} listp_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
