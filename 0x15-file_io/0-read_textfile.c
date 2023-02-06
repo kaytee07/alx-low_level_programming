ba0543887d14f8b48469a8a46eb187cc7a91520a
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-02-06 14:50:32 +0000

    add strlen and putchar to main.h

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,6 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include "main.h"
 
 /**
@@ -10,42 +9,45 @@
  * @letters: chars to read
  * Return: chars read
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t read_bytes, written_bytes;
-char *buf;
-if (!filename)
-{
-return (0);
-}
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-{
-return (0);
-}
-buf = malloc(letters);
-if (!buf)
-{
-close(fd);
-return (0);
-}
-read_bytes = read(fd, buf, letters);
-if (read_bytes == -1)
-{
-free(buf);
-close(fd);
-return (0);
-}
-written_bytes = write(STDOUT_FILENO, buf, read_bytes);
-if (written_bytes == -1 || written_bytes != read_bytes)
-{
-free(buf);
-close(fd);
-return (0);
-}
-free(buf);
-close(fd);
-return (read_bytes);
+	int fd;
+	char *buff;
+	ssize_t bytes, r;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes = read(fd, buff, letters);
+
+	if (bytes == -1)
+	{
+		close(fd);
+		free(buff);
+		return (0);
+	}
+
+	r = write(STDOUT_FILENO, buff, bytes);
+
+	if (r == -1)
+	{
+		close(fd);
+		free(buff);
+		return (0);
+	}
+	close(fd);
+	return (bytes);
 }
