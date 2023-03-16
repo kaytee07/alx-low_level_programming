3d486a8fb292daef6963b23bd6268413ac60b1b7
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-16 17:04:05 +0000

    errors

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,35 +3,41 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - delete new node at index
+ * delete_dnodeint_at_index - delete node at index
  * @head: head of linked list
- * @index: index to insert new node
- * Return: 1 if new node is deleted or -1 if otherwise
+ * @index: index to delete node
+ * Return: 1 if node is deleted or -1 if otherwise
  */
-
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-unsigned int current_index = 0;
-dlistint_t *current_node = *head;
+    unsigned int current_index = 0;
+    dlistint_t *current_node = *head;
 
-if (*head == NULL)
-return (-1);
+    if (*head == NULL)
+        return (-1);
 
+    if (index == 0)
+    {
+        *head = current_node->next;
+        if (current_node->next != NULL)
+            current_node->next->prev = NULL;
+        free(current_node);
+        return (1);
+    }
 
-while (current_node != NULL)
-{
-if (current_node->next != NULL && (current_index + 1) == index)
-{
-dlistint_t *temp = current_node->next->next;
-temp->prev = current_node;
-current_node->next = temp;
-return (1);
-}
-
-current_index++;
-current_node = current_node->next;
-}
-
-return (-1);
+    while (current_node != NULL)
+    {
+        if (current_node->next != NULL && (current_index + 1) == index)
+        {
+            dlistint_t *temp = current_node->next->next;
+            free(current_node->next);
+            current_node->next = temp;
+            if (temp != NULL)
+                temp->prev = current_node;
+            return (1);
+        }
+        current_index++;
+        current_node = current_node->next;
+    }
+    return (-1);
 }
