8f236c56818b0a3d4d239d99fe8f091a185c978f
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-03-20 21:56:46 +0000

    tired

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-LD_PRELOAD="$./libgiga.so"
+wget -P /tmp https://github.com/kaytee07/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$./libgiga.so"
