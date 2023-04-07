be7d8e64233fd3e19c687d8804c67a930bf4274d
kaytee07 <kofi.taylor07@gmail.com>
2023-04-07 15:38:06 +0000

    retrieve data from hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,7 +5,7 @@
  * hash_table_get - retrieve data stored in the hash table
  * @ht: hash table to retrieve data from
  * @key: key used to generate hash_code
- * Return: NULL if failure or value 
+ * Return: NULL if failure or value
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
@@ -15,19 +15,19 @@ hash_node_t *node;
 
 if (ht == NULL || key == NULL || *key == '\0')
 return (NULL);
-  
+
 index = key_index((unsigned char *) key, ht->size);
-  
-if(index >= ht->size)
+
+if (index >= ht->size)
 return (NULL);
 
 node = ht->array[index];
 
-if(node != NULL)
+if (node != NULL)
 {
-if(strcmp(node->key, key) == 0)
+if (strcmp(node->key, key) == 0)
 return (node->value);
 }
- 
+
 return (NULL);
 }
