b5df440cb41b6f8d474b4bbe417517ae62919035
kaytee07 <kofi.taylor07@gmail.com>
2023-05-24 18:36:16 +0000

    finding the perimeter of the island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -19,7 +19,6 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 perimeter += 4
 
-
                 if row > 0 and grid[row - 1][col] == 1:
                     perimeter -= 2
                 if col > 0 and grid[row][col - 1] == 1:
