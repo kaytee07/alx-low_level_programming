06e05d789b2863452b3d53743184773de7a33f22
kaytee07 <kofi.taylor07@gmail.com>
2023-07-12 05:14:53 +0000

    add all

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,10 @@
 #define SEARCH_ALGOS_H
 
 #include <stddef.h>
+#include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int traverse_arr(int *array ,int start, int end, int value);
+int traverse_arr(int *array, int start, int end, int value);
 
 #endif
