f00a0a7435d66f5640301ec5c93372c23854327b
kaytee07 <kofi.taylor07@gmail.com>
2023-07-12 05:46:52 +0000

    binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,53 +1,5 @@
 #include "search_algos.h"
-#include <stdio.h>
 #include <math.h>
-
-/**
- * traverse_arr - search for value using binary searcg
- * @start: part of array set as the beginning to identyfy middle
- * @end: part fo array set as the end used to identify middle
- * @value: value to be searched
- * @array: array to be searched
- * Return: return index of value if found or -1 if failure
- */
-
-int traverse_arr(int *array, int start, int end, int value)
-{
-int middle;
-int i;
-if (start >= end)
-{
-return (-1);
-}
-printf("Searching in array: ");
-for (i = start; i < end; i++)
-{
-if (i == end - 1)
-{
-printf("%d", array[i]);
-}
-else
-{
-printf("%d, ", array[i]);
-}
-}
-printf("\n");
-
-middle = floor((start + end) / 2);
-if (array[middle] == value)
-{
-return (middle);
-}
-
-if (array[middle] > value)
-{
-return (traverse_arr(array, start, middle, value));
-}
-else
-{
-return (traverse_arr(array, middle + 1, end, value));
-}
-}
 
 /**
  * binary_search - sind value in array using binary search algo
@@ -58,9 +10,43 @@ return (traverse_arr(array, middle + 1, end, value));
  */
 int binary_search(int *array, size_t size, int value)
 {
-if (size == 0)
+size_t start = 0;
+size_t end = size - 1;
+
+if (array == NULL || size == 0)
 {
 return (-1);
 }
-return (traverse_arr(array, 0, (int)size, value));
+
+while (start <= end)
+{
+size_t middle = start + (end - start) / 2;
+
+printf("Searching in array: ");
+for (size_t i = start; i <= end; i++)
+{
+if (i == end)
+{
+printf("%d", array[i]);
+}
+else
+{
+printf("%d, ", array[i]);
+}
+}
+printf("\n");
+if (array[middle] == value)
+{
+return ((int)middle);
+}
+else if (array[middle] < value)
+{
+start = middle + 1;
+}
+else
+{
+end = middle - 1;
+}
+}
+return (-1);
 }
